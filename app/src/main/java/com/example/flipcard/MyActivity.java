ab80309a304afd7313da8533e5e736f0f965d24d
Yujin Sung <rnlduadns98@naver.com>
2021-11-28 21:24:12 +0900

    [성유진] Mypage 추가 + 홈버튼

--- app/src/main/java/com/example/flipcard/MyActivity.java
+++ app/src/main/java/com/example/flipcard/MyActivity.java
@@ -7,11 +7,22 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MyActivity extends StartActivity {
+public class MyActivity extends AppCompatActivity {
+
+    Button home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mypage);
+        home = findViewById(R.id.button3);
+        home.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), StartActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
 
 }
