0fe1e28d946cf6d76da903aee2daa06695f5bd91
Kimhanyeol <63261054+Kimhanyeol@users.noreply.github.com>
2021-12-11 20:29:14 +0900

    [한열] 마이페이지 기록

--- app/src/main/java/com/example/flipcard/TimeCount.java
+++ app/src/main/java/com/example/flipcard/TimeCount.java
@@ -2,6 +2,7 @@ package com.example.flipcard;
 
 public class TimeCount {
     private static long count = 0;
+    private static String record ="";
 
     public void setcount(long count){
         this.count = count;
@@ -10,5 +11,9 @@ public class TimeCount {
     public long getcount(){
         return count;
     }
+
+    public void setRecord(String record) {this.record = record;}
+
+    public String getRecord() {return record; }
 
 }
