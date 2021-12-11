a2091025394546a5c46ee042a1d0d9be0c7d9ed3
Kimhanyeol <63261054+Kimhanyeol@users.noreply.github.com>
2021-12-11 20:28:04 +0900

    [한열] 마이페이지 기록

--- app/src/main/java/com/example/flipcard/MyActivity.java
+++ app/src/main/java/com/example/flipcard/MyActivity.java
@@ -4,16 +4,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MyActivity extends AppCompatActivity {
 
     Button home;
+    EditText time;
+    TimeCount hanyeol = new TimeCount();
+    String record = hanyeol.getRecord();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mypage);
+
+        time = findViewById(R.id.editTextTextPersonName2);
+        time.setText(record);
+
         home = findViewById(R.id.button3);
         home.setOnClickListener(new View.OnClickListener(){
             @Override
